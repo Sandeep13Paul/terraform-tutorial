7fb97e9a4dbbb08f0c89136fa4cd5a6a8a4bc4df
sonupaul13 <sonupaul213@gmail.com>
2025-06-23 12:55:22 +0530

    added

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = base64decode(var.google_creds)
+  credentials = var.google_creds
   project     = "sunlit-cab-463104-m6"
   region      = "asia-southeast1"
   zone        = "asia-southeast1-a"
