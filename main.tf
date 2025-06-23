b446355281ed215811fb0407741fc44b06bc9dc9
sonupaul13 <sonupaul213@gmail.com>
2025-06-23 15:50:57 +0530

    added

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("${path.module}/creds.json")
+  credentials = file("/home/atlantis/.atlantis/repos/Sandeep13Paul/terraform-tutorial/21/default/creds.json")
   project     = "sunlit-cab-463104-m6"
   region      = "us-central1"
   zone        = "us-central1-a"
