f70209269e4964c40d84c079a5bef3ea418ebddb
sonupaul13 <sonupaul213@gmail.com>
2025-06-23 15:32:35 +0530

    adde

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = file("/home/atlantis/.atlantis/repos/Sandeep13Paul/terraform-tutorial/21/default/decoded.json")
+  credentials = file("${path.module}/decoded.json")
   project     = "sunlit-cab-463104-m6"
-  region      = "asia-southeast1"
-  zone        = "asia-southeast1-a"
+  region      = "us-central1"
+  zone        = "us-central1-a"
 }
