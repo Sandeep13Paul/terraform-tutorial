95b5bff2cb77139676c57fb68f991b107f6ac3b2
sonupaul13 <sonupaul213@gmail.com>
2025-06-23 15:36:31 +0530

    added

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "google" {
-  credentials = file("/home/atlantis/.atlantis/repos/Sandeep13Paul/terraform-tutorial/21/default/decoded.json")
+  credentials = var.google_creds
   project     = "sunlit-cab-463104-m6"
   region      = "us-central1"
   zone        = "us-central1-a"
+}
+
+variable "google_creds" {
+  description = "GCP credentials JSON"
+  type        = string
 }
