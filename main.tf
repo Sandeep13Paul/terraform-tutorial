8f9a91fd072e856257019d5867e8d1443f80309a
sonupaul13 <sonupaul213@gmail.com>
2025-06-23 12:09:21 +0530

    added

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("key.json")
+  credentials = var.google_creds
   project     = "sunlit-cab-463104-m6"
   region      = "asia-southeast1"
   zone        = "asia-southeast1-a"
