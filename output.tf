ec19c93547b0baafa1ae315c57388de2fbc6d9c9
sonupaul13 <sonupaul213@gmail.com>
2025-06-24 11:28:50 +0530

    added

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "auto_vpc_name" {
-  value = google_compute_network.auto-vpc-tf-unique-name.id
+  value = google_compute_network.auto-vpc-tf-unique-names.id
 }
 
 output "custom_vpc_name" {
-  value = google_compute_network.custom-vpc-tf-unique.id
+  value = google_compute_network.custom-vpc-tf-uniques.id
 }
--- resources.tf
+++ resources.tf
@@ -1,24 +1,24 @@
-resource "google_compute_network" "auto-vpc-tf-unique-name" {
-  name                    = "auto-vpc-tf-unique-name"
+resource "google_compute_network" "auto-vpc-tf-unique-names" {
+  name                    = "auto-vpc-tf-unique-names"
   auto_create_subnetworks = true
 }
 
-resource "google_compute_network" "custom-vpc-tf-unique" {
-  name                    = "custom-vpc-tf-unique"
+resource "google_compute_network" "custom-vpc-tf-uniques" {
+  name                    = "custom-vpc-tf-uniques"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet-custom-vpc-tfv" {
   name    = "subnet-custom-vpc-tfvc"
   region  = "asia-southeast1"
-  network = google_compute_network.custom-vpc-tf-unique.id
+  network = google_compute_network.custom-vpc-tf-uniques.id
   ip_cidr_range = "10.1.0.0/24"
   private_ip_google_access = true
 }
 
 resource "google_compute_firewall" "allow-icmp" {
     name = "allow-icmp-sandeep"
-    network = google_compute_network.custom-vpc-tf-unique.id
+    network = google_compute_network.custom-vpc-tf-uniques.id
     direction = "INGRESS"
     priority = 1000
     source_ranges = ["14.97.94.230/32"]
@@ -28,8 +28,8 @@ resource "google_compute_firewall" "allow-icmp" {
 
 }
 
-resource "google_storage_bucket" "your-bucket-name-sandeep" {
-  name          = "your-bucket-name-sandeep"
+resource "google_storage_bucket" "your-bucket-name-sandeeps" {
+  name          = "your-bucket-name-sandeeps"
   location      = "US"
   force_destroy = true
   storage_class = "NEARLINE"
@@ -66,8 +66,8 @@ resource "google_storage_bucket" "your-bucket-name-sandeep" {
   }
 }
 
-resource "google_storage_bucket_object" "object-sandeep" {
+resource "google_storage_bucket_object" "object-sandeeps" {
     name = "iphone_logo"
-    bucket = google_storage_bucket.your-bucket-name-sandeep.name
+    bucket = google_storage_bucket.your-bucket-name-sandeeps.name
     source = "p2.jpg"
 }
