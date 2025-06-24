f7686f6e88a61d166dbbc877458f515425f11f4e
sonupaul13 <sonupaul213@gmail.com>
2025-06-24 11:21:04 +0530

    added

--- resources.tf
+++ resources.tf
@@ -1,24 +1,24 @@
-resource "google_compute_network" "auto-vpc-tf" {
-  name                    = "auto-vpc-tf"
+resource "google_compute_network" "auto-vpc-tf-unique-name" {
+  name                    = "auto-vpc-tf-unique-name"
   auto_create_subnetworks = true
 }
 
-resource "google_compute_network" "custom-vpc-tf" {
-  name                    = "custom-vpc-tf"
+resource "google_compute_network" "custom-vpc-tf-unique" {
+  name                    = "custom-vpc-tf-unique"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnet-custom-vpc-tf" {
   name    = "subnet-custom-vpc-tfv"
   region  = "asia-southeast1"
-  network = google_compute_network.custom-vpc-tf.id
+  network = google_compute_network.custom-vpc-tf-unique.id
   ip_cidr_range = "10.1.0.0/24"
   private_ip_google_access = true
 }
 
 resource "google_compute_firewall" "allow-icmp" {
     name = "allow-icmp-sandeep"
-    network = google_compute_network.custom-vpc-tf.id
+    network = google_compute_network.custom-vpc-tf-unique.id
     direction = "INGRESS"
     priority = 1000
     source_ranges = ["14.97.94.230/32"]
@@ -29,7 +29,7 @@ resource "google_compute_firewall" "allow-icmp" {
 }
 
 resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
+  name          = "your-bucket-name-sandeep"
   location      = "US"
   force_destroy = true
   storage_class = "NEARLINE"
